0778518869336af00b150f2fa270c6305128e71c
Rune Skovrup Hansen <rsha@mir-robots.com>
2024-06-04 08:18:43 +0200

    Update feature flag documentation

--- src/feature_flag.js
+++ src/feature_flag.js
@@ -3,7 +3,7 @@ export function feature_flag(feature_flag, element) {
 }
 
 /* Application feature flags. Set a flag to true
-to enable the corresponding feature. */
+to enable the feature. */
 
 export const flag_perks = true;
 export const flag_maps = false;
